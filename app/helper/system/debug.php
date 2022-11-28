0d210e296e4c0e0038d4c009c938dbbdf9343278
Paul Andrianoff <pandrianoff@gmail.com>
2022-11-28 20:16:15 +0100

    add style to dd

--- app/helper/system/debug.php
+++ app/helper/system/debug.php
@@ -1,3 +1,20 @@
+<style>
+    pre.xdebug-var-dump {
+    background-color: #18171B;
+    color: #FF8400;
+    line-height: 1.2em;
+    font: 12px Menlo, Monaco, Consolas, monospace;
+    word-wrap: break-word;
+    position: relative;
+    z-index: 99999;
+    word-break: break-all;
+    display: block;
+    white-space: pre;
+    padding: 5px;
+    overflow: initial !important;
+}
+</style>
+
 <?php
 /**
  * Here you find all functions for debugging
